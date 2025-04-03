3dac20f9b6329e2e91f7152e18239f484880edd1
Dianyi Yang <kv9898@rubuky.com>
2025-04-04 01:20:01 +0800

    Remove development R files

--- R/code.R
+++
@@ -1,9 +0,0 @@
-installed <- installed.packages()
-jsonlite::write_json(installed, "R/installed.json")
-
-loaded <- loadedNamespaces()
-jsonlite::write_json(loaded, "R/loaded.json")
-
-data <- as.data.frame(installed.packages()[, c("Package", "Version")])
-loaded <- loadedNamespaces()
-data$loaded <- ifelse(data$Package %in% loaded, TRUE, FALSE)
