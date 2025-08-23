c0d6832df937d68cc2f96d7f27e75378c711bf24
Dianyi Yang <dianyi.yang@politics.ox.ac.uk>
2025-08-23 15:27:11 +0800

    Fix github code

--- src/install.ts
+++ src/install.ts
@@ -132,9 +132,8 @@ async function installFromGithub(libPath: string): Promise<void> {
 
     const rCode =
         installer === "pak"
-            ? `pak::pkg_install(${esc(repo.trim())}${libOpt}, ask = FALSE, upgrade = FALSE)`
-            : `withr::with_libpaths(${libPath ? `"${esc(libPath)}"` : ".libPaths()[1]"}, 
-          devtools::install_github(${esc(repo.trim())}))`;
+            ? `pak::pkg_install("${esc(repo.trim())}"${libOpt})`
+            : `withr::with_libpaths(${libPath ? `"${esc(libPath)}"` : ".libPaths()[1]"}, devtools::install_github("${esc(repo.trim())}"))`;
 
     const observer = getObserver("Error while installing from {0}: {1}", [repo]);
     await positron.runtime.executeCode(
